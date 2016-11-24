621cd6e8ee46f2f681344540ac521890b13eb13f
AndreKla <a.klassen78@gmx.de>
2016-11-24 15:43:44 +0100

    Map wurde angepackt

--- immobilia/map.php
+++ immobilia/map.php
@@ -1,4 +1,3 @@
-<!-- TODO: Investigate this broken page -->
 <?php
     
     session_start();
@@ -11,12 +10,11 @@
 
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <!-- Meta, title, CSS, favicons, etc. -->
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Gentellela Alela! | </title>
+  <title>MAP Übersicht </title>
 
   <!-- Bootstrap core CSS -->
 
@@ -32,15 +30,6 @@
 
   <script src="js/jquery.min.js"></script>
 
-  <!--[if lt IE 9]>
-        <script src="../assets/js/ie8-responsive-file-warning.js"></script>
-        <![endif]-->
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 
 </head>
 
