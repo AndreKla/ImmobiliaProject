3a5250c141fb03e53771ebcc521f2301e6103ab6
AndreKla <a.klassen78@gmx.de>
2016-11-25 15:40:48 +0100

    toggle fix

--- immobilia/menu.php
+++ immobilia/menu.php
@@ -11,27 +11,13 @@ public static function createMenu($titel) {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title></title>
-
 	<?php   include('includes_css.php'); ?>
-
-  </head>
-
 
  <body class="nav-md footer_fixed">
     <div class="container body">
 
 
-<div class="main_container">
+<div class="main_container" style="overflow:hidden;">
 <div class="col-md-3 left_col">
   <div class="left_col scroll-view">
 	<div class="navbar nav_title" style="border: 0;">
