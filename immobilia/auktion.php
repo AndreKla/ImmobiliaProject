3d359082a38957ce8304474e85c1eb7837a085cc
AndreKla <a.klassen78@gmx.de>
2016-12-05 14:00:01 +0100

    auktion angepasst

--- immobilia/auktion.php
+++ immobilia/auktion.php
@@ -53,15 +53,14 @@
 
                       <div class="">
                         <div class="product_price">
-                          <h1 class="price">Ksh80.00</h1>
-                          <span class="price-tax">Ex Tax: Ksh80.00</span>
+                          <h1 class="price">€ 80.00</h1>
+                          <span class="price-tax">Tax: Ksh80.00</span>
                           <br>
                         </div>
                       </div>
 
                       <div class="">
-                        <button type="button" class="btn btn-default btn-lg">Add to Cart</button>
-                        <button type="button" class="btn btn-default btn-lg">Add to Wishlist</button>
+                        <button type="button" class="btn btn-default btn-lg">Bieten</button>
                       </div>
                     </div>
                   </div>
@@ -93,7 +92,7 @@
 
 						  <div class="col-md-6 col-sm-6 col-xs-12">
 							<p>Grid with slider labels are far away outside it's container</p>
-							<input type="text" id="range_25" value="" name="range" />
+							<input type="text" id="range_26" value="" name="range" />
 						  </div>				  
 						 					  
                     </div>
@@ -134,25 +133,6 @@
 		<!-- Ion.RangeSlider -->
     <script>
       $(document).ready(function() {
-        $("#range_27").ionRangeSlider({
-          type: "double",
-          min: 1000000,
-          max: 2000000,
-          grid: true,
-          force_edges: true
-        });
-        $("#range").ionRangeSlider({
-          hide_min_max: true,
-          keyboard: true,
-          min: 0,
-          max: 5000,
-          from: 1000,
-          to: 4000,
-          type: 'double',
-          step: 1,
-          prefix: "$",
-          grid: true
-        });
         $("#range_25").ionRangeSlider({
           type: "double",
           min: 1000000,
@@ -167,35 +147,9 @@
           grid: true,
           grid_snap: true
         });
-        $("#range_31").ionRangeSlider({
-          type: "double",
-          min: 0,
-          max: 100,
-          from: 30,
-          to: 70,
-          from_fixed: true
-        });
-        $(".range_min_max").ionRangeSlider({
-          type: "double",
-          min: 0,
-          max: 100,
-          from: 30,
-          to: 70,
-          max_interval: 50
-        });
-        $(".range_time24").ionRangeSlider({
-          min: +moment().subtract(12, "hours").format("X"),
-          max: +moment().format("X"),
-          from: +moment().subtract(6, "hours").format("X"),
-          grid: true,
-          force_edges: true,
-          prettify: function(num) {
-            var m = moment(num, "X");
-            return m.format("Do MMMM, HH:mm");
-          }
-        });
       });
     </script>
+	
     <!-- /Ion.RangeSlider -->
 
 
